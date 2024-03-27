3eac408f25617c2a80fd075498e14f59a5d6dc7a
joyce moura <mileniorapper@gmail.com>
2024-03-27 11:48:32 -0300

    add: funcoes para localhost

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,60 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const contactForm = document.getElementById("contactForm");
+  const messageList = document.getElementById("messageList");
+  const clearMessagesButton = document.getElementById("clearMessages");
+
+  contactForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      const formData = new FormData(contactForm);
+      const messageObject = {};
+      formData.forEach(function(value, key){
+          messageObject[key] = value;
+      });
+
+      let messages = JSON.parse(localStorage.getItem("messages")) || [];
+      messages.push(messageObject);
+      localStorage.setItem("messages", JSON.stringify(messages));
+
+      displayMessages();
+      contactForm.reset();
+  });
+
+  clearMessagesButton.addEventListener("click", function() {
+      localStorage.removeItem("messages");
+      messageList.innerHTML = "";
+  });
+
+  function displayMessages() {
+    messageList.innerHTML = "";
+    let messages = JSON.parse(localStorage.getItem("messages")) || [];
+    messages.forEach(function(message) {
+        const messageContainer = document.createElement("div");
+        messageContainer.classList.add("message-item");
+        
+        const namePara = document.createElement("p");
+        namePara.textContent = `Nome: ${message.name}`;
+        messageContainer.appendChild(namePara);
+        
+        const emailPara = document.createElement("p");
+        emailPara.textContent = `Email: ${message.email}`;
+        messageContainer.appendChild(emailPara);
+        
+        const phonePara = document.createElement("p");
+        phonePara.textContent = `Telefone: ${message.phone}`;
+        messageContainer.appendChild(phonePara);
+        
+        const messagePara = document.createElement("p");
+        messagePara.textContent = `Mensagem: ${message.message}`;
+        messageContainer.appendChild(messagePara);
+        
+        messageList.appendChild(messageContainer);
+    });
+}
+
+  
+  displayMessages();
+})
